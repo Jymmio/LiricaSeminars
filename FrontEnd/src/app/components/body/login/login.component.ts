94586bda966a13c93147a7d523919484b388933d
b19025326 <nadjim.bouzidi@etu.univ-amu.fr>
2024-04-18 08:40:27 +0200

    added method to get login inputs values

--- FrontEnd/src/app/components/body/login/login.component.ts
+++ FrontEnd/src/app/components/body/login/login.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
+  loginAction(): void {
+    const emailControl = this.form.get('email');
+    const passwordControl = this.form.get('password');
+
+    if (emailControl) {
+      const email = emailControl.value;
+      console.log("adresse mail : "+email);
+    } else {
+      console.log('Email control is not found');
+    }
+    if(passwordControl){
+      const password = passwordControl.value;
+      console.log("mot de passe : " + password);
+    } else {
+      console.log("Password control is not found");
+    }
+  }
 }
